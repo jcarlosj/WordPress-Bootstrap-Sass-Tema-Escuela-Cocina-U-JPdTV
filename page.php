8c712b194dd0372a4ce1f8f9be626041eb5dc1ea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-05-20 09:25:13 -0500

    Page(F): page.php

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 outstanding-image outstanding-image-us"></div>
+        </div><!-- .row -->
+    </div><!-- .container -->
+
+    <main class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 px-5 py-3 content content-us">
+                <h1 class="text-center my-5 separator">Nosotros</h1>
+                <p>Ipsa molestiae sit tempora nemo ratione rerum ab, iste minima qui velit dicta, quaerat adipisci obcaecati laborum! Tempora explicabo id doloremque ad!</p>
+                <p>Iusto, assumenda neque. Sit aperiam neque doloribus eveniet deleniti consequuntur, ullam in ea suscipit nesciunt, quidem assumenda placeat corrupti labore, autem laudantium.</p>
+                <p>Aperiam, dicta. Nulla ullam architecto, eligendi expedita aperiam iure corrupti quasi id pariatur voluptatum libero dolores iusto voluptatibus, reiciendis odio repudiandae accusamus.</p>
+            </div><!-- .col-8 -->
+        </div><!-- .row -->
+
+        <section class="study-with-us my-5">
+            <h2 class="text-center my-5 separator">¿Por qué estudiar con nosotros?</h2>
+            <div class="row">
+                <div class="col-md-4 text-center study-with-us__info">
+                    <img class="img-fluid mb-3" src="<?php echo get_template_directory_uri() .'/dist/assets/images/icono_chef.png'; ?>" alt="Chef's Internacionales">
+                    <h3>Chef's Internacionales</h3>
+                    <p>Todo el conocimiento es impartido por los Chef's más destacados de esta industria, reconocidos internacionalmente.</p>
+                </div><!-- .col-md-4 -->
+                <div class="col-md-4 text-center study-with-us__info">
+                    <img class="img-fluid mb-3" src="<?php echo get_template_directory_uri() .'/dist/assets/images/icono_vino.png'; ?>" alt="Todo sobre bebidas">
+                    <h3>Todo sobre bebidas</h3>
+                    <p>Aprenderás de catadores certificados expertos en bebidas cual es el mejor acompañante para cada plato.</p>
+                </div><!-- .col-md-4 -->   
+                <div class="col-md-4 text-center study-with-us__info">
+                    <img class="img-fluid mb-3" src="<?php echo get_template_directory_uri() .'/dist/assets/images/icono_menu.png'; ?>" alt="Menú del mundo">
+                    <h3>Platillos y técnicas</h3>
+                    <p>El programa actualiza constantemente el recetario y las técnicas. Toda la puesta en escena de la gastronomía mundial.</p>
+                </div><!-- .col-md-4 -->
+            </div>
+        </section>
+    </main><!-- .container -->    
+
+<?php get_footer(); ?>
